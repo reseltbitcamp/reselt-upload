dfe257910e6b25c0fa00c28baf8ce5bbc1534fac
yeonsubak <yeonsubak@gmail.com>
2022-07-28 17:17:36 +0900

    사진 넣었음

--- src/main/java/upload/controller/UploadController.java
+++ src/main/java/upload/controller/UploadController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,11 +20,10 @@ import style.service.StyleService;
 
 @Controller
 public class UploadController {
-	@Autowired
-	private StyleService styleService;
 	
-	@PostMapping(value = "/")
-	public ResponseEntity<?> upload(@ModelAttribute StyleDTO styleDTO,
+	@CrossOrigin(origins = "http://localhost:8080")
+	@PostMapping(value = "style")
+	public ResponseEntity<?> uploadStyle(@ModelAttribute StyleDTO styleDTO,
 									@RequestParam MultipartFile img,
 									HttpSession session) {
 		String filePath = session.getServletContext().getRealPath("/resources/img/style");
@@ -37,8 +38,25 @@ public class UploadController {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
 		
-		styleDTO.setStyle_image(fileName);
-		styleService.styleWriteForm(styleDTO);
+		return ResponseEntity.ok("이미지를 업로드하였습니다.");
+	}
+	
+	@CrossOrigin(origins = "http://localhost:8080")
+	@PostMapping(value = "myPage")
+	public ResponseEntity<?> uploadProfile(@ModelAttribute StyleDTO styleDTO,
+									@RequestParam MultipartFile img,
+									HttpSession session) {
+		String filePath = session.getServletContext().getRealPath("/resources/img/myPage");
+		String fileName = img.getOriginalFilename();
+		
+		File file = new File(filePath, fileName);
+		System.out.println(filePath);
+		
+		try {
+			img.transferTo(file);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 		
 		return ResponseEntity.ok("이미지를 업로드하였습니다.");
 	}
